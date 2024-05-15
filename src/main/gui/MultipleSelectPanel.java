9619296987977113968a5f66eb862da0412518bd
Abdelrahman Mostafa <abdomody35@gmail.com>
2024-05-15 17:18:44 +0300

    used arraylist instead of array to avoid warning

--- src/main/gui/MultipleSelectPanel.java
+++ src/main/gui/MultipleSelectPanel.java
@@ -6,25 +6,27 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import java.util.ArrayList;
+
 public class MultipleSelectPanel extends JPanel
 {
     JLabel label;
-    JComboBox<Object> list[];
+    ArrayList<JComboBox<Object>> list;
     
     public MultipleSelectPanel(String label, Object[]... values)
     {
         super(new FlowLayout(FlowLayout.LEFT));
         this.label = new JLabel(label);
         this.add(this.label);
-        this.list = new JComboBox[values.length];
+        this.list = new ArrayList<JComboBox<Object>>();
         for (int i = 0; i < values.length; i++)
         {
-            this.list[i] = new JComboBox<>(values[i]);
-            this.add(this.list[i]);
+            this.list.add(new JComboBox<>(values[i]));
+            this.add(this.list.get(i));
         }
     }
 
-    public JComboBox<Object>[] getList()
+    public ArrayList<JComboBox<Object>> getList()
     {
         return this.list;
     }
