1dc1e1bbd6de4cdcedba9676d94de49529f48ba4
Abdelrahman Mostafa <abdomody35@gmail.com>
2024-05-18 20:44:38 +0300

    Changed the order of the tests for PlantTest

--- src/test/integrationTests/PlantTest.java
+++ src/test/integrationTests/PlantTest.java
@@ -26,6 +26,46 @@ public class PlantTest extends Plant
     }
 
     @Test
+    public void SetWateredAndGetWateredIntegration() {
+        this.setWatered("9 am", "Wednesday", "45 min");
+        ArrayList<DayHr> watering = this.getWatered();
+        assertEquals(1, watering.size());
+        assertEquals("9 am", watering.get(0).getTime());
+        assertEquals("Wednesday", watering.get(0).getDay());
+        assertEquals("45 min", watering.get(0).getDuration());
+    }
+
+    @Test
+    public void SetWateredDayHrAndGetWateredIntegration() {
+        this.setWatered(new DayHr("9 am", "Wednesday", "45 min"));
+        ArrayList<DayHr> watering = this.getWatered();
+        assertEquals(1, watering.size());
+        assertEquals("9 am", watering.get(0).getTime());
+        assertEquals("Wednesday", watering.get(0).getDay());
+        assertEquals("45 min", watering.get(0).getDuration());
+    }
+
+    @Test
+    public void AddWateredAndGetWateredIntegration() {
+        this.addWatered("7 am", "Tuesday", "20 min");
+        ArrayList<DayHr> watering = this.getWatered();
+        assertEquals(1, watering.size());
+        assertEquals("7 am", watering.get(0).getTime());
+        assertEquals("Tuesday", watering.get(0).getDay());
+        assertEquals("20 min", watering.get(0).getDuration());
+    }
+
+    @Test
+    public void AddWateredDayHrAndGetWateredIntegration() {
+        this.addWatered(new DayHr("7 am", "Tuesday", "20 min"));
+        ArrayList<DayHr> watering = this.getWatered();
+        assertEquals(1, watering.size());
+        assertEquals("7 am", watering.get(0).getTime());
+        assertEquals("Tuesday", watering.get(0).getDay());
+        assertEquals("20 min", watering.get(0).getDuration());
+    }
+
+    @Test
     public void SetColorAndSetWateredIntegration() {
         this.setColor("blue");
         this.setWatered("9 am", "Wednesday", "45 min");
@@ -66,46 +106,6 @@ public class PlantTest extends Plant
         this.setColor("blue");
         this.addWatered(new DayHr("7 am", "Tuesday", "20 min"));
         assertEquals("blue", this.getColor());
-        ArrayList<DayHr> watering = this.getWatered();
-        assertEquals(1, watering.size());
-        assertEquals("7 am", watering.get(0).getTime());
-        assertEquals("Tuesday", watering.get(0).getDay());
-        assertEquals("20 min", watering.get(0).getDuration());
-    }
-
-    @Test
-    public void SetWateredAndGetWateredIntegration() {
-        this.setWatered("9 am", "Wednesday", "45 min");
-        ArrayList<DayHr> watering = this.getWatered();
-        assertEquals(1, watering.size());
-        assertEquals("9 am", watering.get(0).getTime());
-        assertEquals("Wednesday", watering.get(0).getDay());
-        assertEquals("45 min", watering.get(0).getDuration());
-    }
-
-    @Test
-    public void SetWateredDayHrAndGetWateredIntegration() {
-        this.setWatered(new DayHr("9 am", "Wednesday", "45 min"));
-        ArrayList<DayHr> watering = this.getWatered();
-        assertEquals(1, watering.size());
-        assertEquals("9 am", watering.get(0).getTime());
-        assertEquals("Wednesday", watering.get(0).getDay());
-        assertEquals("45 min", watering.get(0).getDuration());
-    }
-
-    @Test
-    public void AddWateredAndGetWateredIntegration() {
-        this.addWatered("7 am", "Tuesday", "20 min");
-        ArrayList<DayHr> watering = this.getWatered();
-        assertEquals(1, watering.size());
-        assertEquals("7 am", watering.get(0).getTime());
-        assertEquals("Tuesday", watering.get(0).getDay());
-        assertEquals("20 min", watering.get(0).getDuration());
-    }
-
-    @Test
-    public void AddWateredDayHrAndGetWateredIntegration() {
-        this.addWatered(new DayHr("7 am", "Tuesday", "20 min"));
         ArrayList<DayHr> watering = this.getWatered();
         assertEquals(1, watering.size());
         assertEquals("7 am", watering.get(0).getTime());
